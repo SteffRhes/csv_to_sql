4f9bf77db42eca34674dba969a2589bc4cbf5595
SteffRhes <steffrhes@pm.me>
2024-06-08 23:32:12 +0200

    working automatic delimiter detection

--- src/csv_to_sql.py
+++ src/csv_to_sql.py
@@ -2,6 +2,8 @@ import csv
 import sqlite3
 
 
+CHECK_DELIMITERS = [",", ";", "\t"]
+CHECK_SAMPLE_LIMIT = 100
 CSV_FILE = "test1.csv"
 DATA_ID = CSV_FILE.split(".csv")[0]
 SHOULD_READ_HEADER = True
@@ -10,12 +12,36 @@ SHOULD_READ_HEADER = True
 def sanitize_name(name):
     return "\"" + name.replace("\"", "\"\"") + "\""
 
+def get_delimiter():
+    with open(CSV_FILE, "r") as f:
+        delimiter_count_all = {d: {} for d in CHECK_DELIMITERS}
+        for i, row in enumerate(f):
+            if i == CHECK_SAMPLE_LIMIT:
+                break
+            for delimiter, delimiter_count_rows in delimiter_count_all.items():
+                count = row.count(delimiter)
+                # TODO: what about files where there is only one column and no delimiter?
+                if count != 0:
+                    delimiter_count_rows[count] = delimiter_count_rows.get(count, 0) + 1
+                    delimiter_count_all[delimiter] = delimiter_count_rows
+
+        highest_occurrence_number = 0
+        highest_occurrence_delimiter = None
+        for delimiter, delimiter_count_rows in delimiter_count_all.items():
+            for _, count in delimiter_count_rows.items():
+                if count > highest_occurrence_number:
+                    highest_occurrence_number = count
+                    highest_occurrence_delimiter = delimiter
+
+    print(f"found delimiter: {highest_occurrence_delimiter.__repr__()}")
+    return highest_occurrence_delimiter
+
 
 with open(CSV_FILE, "r") as f:
     table_name = sanitize_name(DATA_ID)
     db_file = DATA_ID + ".db"
 
-    csv_reader = csv.reader(f)
+    csv_reader = csv.reader(f, delimiter=get_delimiter())
 
     conn = sqlite3.connect(db_file)
     conn.cursor().execute(f"DROP TABLE IF EXISTS {table_name}")
@@ -25,13 +51,13 @@ with open(CSV_FILE, "r") as f:
 
         if line_number == 0:
             max_col_len = len(row)
-            headers = ["line_number INTEGER"]
             if SHOULD_READ_HEADER:
-                headers += row[1:]
+                headers = row[1:]
             else:
-                headers += [f"col_{i + 1}" for i in range(max_col_len)]
+                headers = [f"col_{i + 1}" for i in range(max_col_len - 1)]
 
             headers_sanitized = ", ".join([sanitize_name(h) for h in headers])
+            headers_sanitized = "line_number INTEGER, " + headers_sanitized
             conn.cursor().execute(f"CREATE TABLE {table_name} ({headers_sanitized})")
 
             # cursor = conn.cursor()
@@ -43,7 +69,6 @@ with open(CSV_FILE, "r") as f:
             if SHOULD_READ_HEADER:
                 continue
 
-
         col_number_diff = max_col_len - len(row)
 
         if col_number_diff > 0:
@@ -52,7 +77,7 @@ with open(CSV_FILE, "r") as f:
 
         elif col_number_diff < 0:
             for i in range(max_col_len, len(row)):
-                conn.cursor().execute(f"ALTER TABLE {table_name} ADD COLUMN col_{i + 1}")
+                conn.cursor().execute(f"ALTER TABLE {table_name} ADD COLUMN col_{i}")
             max_col_len = len(row)
 
         place_holder = ("?, " * max_col_len)[:-2]
@@ -62,6 +87,7 @@ with open(CSV_FILE, "r") as f:
     # cursor = conn.cursor()
     # cursor.execute(f"SELECT * FROM {table_name}")
     # print(list(cursor.fetchall()))
+
     conn.commit()
     conn.close()
 
