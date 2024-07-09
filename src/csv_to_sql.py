d92b093d0f39a9cc6275b033ffc49c2115d685fe
SteffRhes <steffrhes@pm.me>
2024-07-09 13:36:41 +0700

    implement type casting

--- src/csv_to_sql.py
+++ src/csv_to_sql.py
@@ -16,6 +16,7 @@ SHOULD_READ_HEADER = True
 def sanitize_name(name):
     return "\"" + name.replace("\"", "\"\"") + "\""
 
+
 def get_delimiter():
     with open(CSV_FILE, "r") as f:
         delimiter_count_all = {d: {} for d in CHECK_DELIMITERS}
@@ -40,56 +41,69 @@ def get_delimiter():
     return highest_occurrence_delimiter
 
 
-with open(CSV_FILE, "r") as f:
-    table_name = sanitize_name(DATA_ID)
-    db_file = DATA_ID + ".db"
+def cast_to_types(row):
+    row_casted = []
+    for col in row:
+        col_casted = col
+        try:
+            col_casted = float(col)
+        except:
+            pass
+        try:
+            col_casted = int(col)
+        except:
+            pass
+        row_casted.append(col_casted)
+    return row_casted
 
-    csv_reader = csv.reader(f, delimiter=get_delimiter())
 
-    conn = sqlite3.connect(db_file)
-    conn.cursor().execute(f"DROP TABLE IF EXISTS {table_name}")
+def main():
+    with open(CSV_FILE, "r") as f:
+        table_name = sanitize_name(DATA_ID)
+        db_file = DATA_ID + ".db"
 
-    for line_number, row in enumerate(csv_reader):
-        row = [line_number + 1] + row
+        csv_reader = csv.reader(f, delimiter=get_delimiter())
 
-        if line_number == 0:
-            max_col_len = len(row)
-            if SHOULD_READ_HEADER:
-                headers = row[1:]
-            else:
-                headers = [f"col_{i + 1}" for i in range(max_col_len - 1)]
+        conn = sqlite3.connect(db_file)
+        conn.cursor().execute(f"DROP TABLE IF EXISTS {table_name}")
 
-            headers_sanitized = ", ".join([sanitize_name(h) for h in headers])
-            headers_sanitized = "line_number INTEGER, " + headers_sanitized
-            conn.cursor().execute(f"CREATE TABLE {table_name} ({headers_sanitized})")
+        for line_number, row in enumerate(csv_reader):
+            row = [line_number + 1] + row
 
-            # cursor = conn.cursor()
-            # cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-            # print(list(cursor.fetchall()))
-            # cursor.execute(f"PRAGMA table_info({table_name})")
-            # print(list(cursor.fetchall()))
+            if line_number == 0:
+                max_col_len = len(row)
+                if SHOULD_READ_HEADER:
+                    headers = row[1:]
+                else:
+                    headers = [f"col_{i + 1}" for i in range(max_col_len - 1)]
+                headers_sanitized = ", ".join([sanitize_name(h) for h in headers])
+                headers_sanitized = "line_number INTEGER PRIMARY KEY, " + headers_sanitized
 
-            if SHOULD_READ_HEADER:
-                continue
+                conn.cursor().execute(f"CREATE TABLE {table_name} ({headers_sanitized})")
 
-        col_number_diff = max_col_len - len(row)
-        if col_number_diff > 0:
-            for i in range(col_number_diff):
-                row.append(None)
-        elif col_number_diff < 0:
-            for i in range(max_col_len, len(row)):
-                conn.cursor().execute(f"ALTER TABLE {table_name} ADD COLUMN col_{i}")
-            max_col_len = len(row)
+                if SHOULD_READ_HEADER:
+                    continue
 
-        place_holder = "?, " * max_col_len
-        place_holder = place_holder[:-2]
+            col_number_diff = max_col_len - len(row)
+            if col_number_diff > 0:
+                for i in range(col_number_diff):
+                    row.append(None)
+            elif col_number_diff < 0:
+                for i in range(max_col_len, len(row)):
+                    conn.cursor().execute(f"ALTER TABLE {table_name} ADD COLUMN col_{i}")
+                max_col_len = len(row)
 
-        conn.cursor().execute(f"INSERT INTO {table_name} VALUES ({place_holder})", row)
+            row = cast_to_types(row)
 
-    # cursor = conn.cursor()
-    # cursor.execute(f"SELECT * FROM {table_name}")
-    # print(list(cursor.fetchall()))
+            place_holder = "?, " * max_col_len
+            place_holder = place_holder[:-2]
 
-    conn.commit()
-    conn.close()
+            conn.cursor().execute(f"INSERT INTO {table_name} VALUES ({place_holder})", row)
+
+        conn.commit()
+        conn.close()
+
+
+if __name__ == "__main__":
+    main()
 
