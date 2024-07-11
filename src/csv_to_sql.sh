f6199f70efcf5b94a3884c967182006b7feb1be5
SteffRhes <steffrhes@pm.me>
2024-07-11 11:08:14 +0700

    add history and persitence handling to script

--- src/csv_to_sql.sh
+++ src/csv_to_sql.sh
@@ -2,27 +2,27 @@
 
 csv_file=""
 db_file=""
+db_history_file=""
 db_name=""
 should_persist=0
 should_skip_headers=0
 
 
 handle_help() {
-  echo "additional args: --persist --skip-headers"
+  echo "first argument: csv file, additional arguments: --persist --skip-headers"
   exit 1
 }
 
 
 if [[ $# -eq 0 || ( $# -eq 1 && $1 == "--help" ) ]]; then 
-
   handle_help
-
 else
 
   # file handling
   csv_file="$1"
   db_name="${csv_file%.*}"
   db_file="$db_name".sqlite
+  db_history_file="$db_name"_history.txt
   shift 1
 
   # other args handling
@@ -40,13 +40,21 @@ else
 
 fi
 
-python3 /root/csv_to_sql.py "$csv_file" "$db_file" "$db_name" "$should_skip_headers"
+if ! [ -e "$db_file" ]; then
+  python3 /root/csv_to_sql.py "$csv_file" "$db_file" "$db_name" "$should_skip_headers"
+fi
+
+if [ -e "$db_history_file" ]; then
+  cp "$db_history_file" /root/.sqlite_history
+fi
 
 sqlite3 "$db_file" "SELECT * FROM ${db_name};" | less
 
 sqlite3 "$db_file" -cmd "SELECT name, sql FROM sqlite_schema WHERE name=\"${db_name}\";"
 
+cp /root/.sqlite_history "$db_history_file"
+
 if [[ "$should_persist" -eq 0 ]]; then
-  rm "$db_file"
+  rm "$db_file" "$db_history_file"
 fi
 
